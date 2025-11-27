c0de1239ac2be0f4fe3259be4a0e526460929e90
Harshit <workwithvrz@gmail.com>
2025-11-27 20:36:41 +0530

    refactor(models): add User → Account relationship mapping
    This update introduces the user_id foreign key and the corresponding SQLAlchemy relationship on the Account model.

--- app/db/models/account.py
+++ app/db/models/account.py
@@ -7,6 +7,8 @@ from decimal import Decimal
 from sqlalchemy import (
     Numeric, Enum, TIMESTAMP
 )
+from sqlalchemy import ForeignKey
+
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import (
     Mapped, mapped_column, relationship
@@ -17,17 +19,18 @@ from app.db.models.transaction import Transaction
 from app.db.models.ledger_entry import LedgerEntry
 from app.db.base import Base
 from app.db.enums import CurrencyCode, AccountStatus
+from app.db.models.user import User
 
 class Account(Base):
     __tablename__ = "accounts"
 
     id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    user_id: Mapped[Optional[UUID]] = mapped_column(UUID(as_uuid=True))
+    user_id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
     currency: Mapped[CurrencyCode] = mapped_column(Enum(CurrencyCode), default=CurrencyCode.INR ,nullable=False)
     balance: Mapped[Decimal] = mapped_column(Numeric(20, 6), nullable=False, default=0)
     status: Mapped[AccountStatus] = mapped_column(Enum(AccountStatus), default=AccountStatus.ACTIVE)
     created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
-
+    user: Mapped["User"] = relationship(back_populates="accounts")
     # Reverse relationships
     sent_transactions: Mapped[List["Transaction"]] = relationship(
         back_populates="sender_account_obj",

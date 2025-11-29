7310fbcab092c65b3d15c0dfc85a1c560f77bc56
Harshit <workwithvrz@gmail.com>
2025-11-29 18:12:11 +0530

    test(factories): add base AccountFactory in account_factory.py  for generating test accounts

--- tests/factories/account_factory.py
+++ tests/factories/account_factory.py
@@ -7,3 +7,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.models.account import Account
 from app.db.enums import CurrencyCode, AccountStatus
 from app.db.models.user import User
+
+class AccountFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = Account
+        sqlalchemy_session_persistence = "flush"  
+    # Fixed defaults
+    id = factory.LazyFunction(uuid4)
+    currency = CurrencyCode.INR
+    balance = Decimal("0.0")
+    status = AccountStatus.ACTIVE
+    created_at = factory.LazyFunction(datetime.utcnow)
+
+    user_id = None  

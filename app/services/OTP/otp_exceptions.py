0e037aee41e586bff3038efb4a6ecaaf8fa7f5af
Harshit <workwithvrz@gmail.com>
2025-12-12 19:15:05 +0530

    refactor: move all explicit exceptions to otp_exceptions.py

---
+++ app/services/OTP/otp_exceptions.py
@@ -0,0 +1,17 @@
+class OTPRateLimitExceeded(Exception):
+    """Raised when the user exceeds the allowed OTP request limit."""
+
+class OTPException(Exception):
+    """Base OTP exception."""
+
+class OTPTooManyRequests(OTPException):
+    """Raised when user requests OTP too frequently."""
+
+class OTPLocked(OTPException):
+    """Raised when phone is temporarily locked due to too many failed verifications."""
+
+class OTPExpired(OTPException):
+    """Raised when OTP has expired."""
+
+class OTPMismatch(OTPException):
+    """Raised when OTP does not match."""
